b3d36dc3a692eecb1032d17622767a2f6b77ac25
mahii060 <mdmahebul-27-2020518110@ier.du.ac.bd>
2024-10-22 14:26:07 +0600

    Sign out implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import app from './firebase/firebase.config';
 
 function App() {
@@ -34,12 +34,23 @@ function App() {
       })
   }
 
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        console.log('Sign out successfully');
+        setUser(null)
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
   return (
     <>
 
       <h1>Firebase + React</h1>
       <button onClick={handleGoogleSignIn}>Google Sign In</button>
       <button onClick={handleGithubSignIn}>Github Sign In</button>
+      <button onClick={handleSignOut}>Sign Out</button>
       {user && <div className="card">
         <h2>User Name: {user.displayName}</h2>
         <p>Email: {user.email}</p>
